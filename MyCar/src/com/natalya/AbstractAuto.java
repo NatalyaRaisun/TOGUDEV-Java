11ae7bc14d5a942104014845eac65dfc38cea923
Natalya Dolzhenko <nata11madona@mail.ru>
2018-11-22 17:52:34 +1000

    Abstract class

--- MyCar/src/com/natalya/AbstractAuto.java
+++ MyCar/src/com/natalya/AbstractAuto.java
@@ -4,13 +4,35 @@ public abstract class AbstractAuto{
 
     private String brand;
     private int year;
-    private String colour = "Black";
-    private int speedMax = 1;
-
+    private String colour;
+    private int speedMax;
+    
     public AbstractAuto(String brand, int year, String colour, int speedMax) {
         this.brand = brand;
         this.year = year;
         this.colour = colour;
         this.speedMax = speedMax;
+
+
     }
+
+    public String getBrand() {
+        return brand;
+    }
+
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public String getColour() {
+        return "Black";
+    }
+
+
+    public int getSpeedMax() {
+        return 1;
+    }
+
 }
